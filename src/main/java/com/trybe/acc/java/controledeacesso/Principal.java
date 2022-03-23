a1b5246a31cdaf4be34e28bef84d4d8050484f2c
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-03-23 20:35:18 -0300

    finish

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal {
@@ -19,42 +20,46 @@ public class Principal {
       System.out.println("1 - Acessar o estabelecimento");
       System.out.println("2 - Finalizar sistema e mostar relatório");
       opcao = Integer.parseInt(scanner.next());
+      switch (opcao) {
+        case 1:
+          System.out.println("Entre com a sua idade:");
+          int idade = Integer.parseInt(scanner.next());
+          String frase = "";
 
-      if (opcao == 1) {
-        System.out.println("Entre com a sua idade:");
-        int idade = Integer.parseInt(scanner.next());
-        String frase = "";
+          if (idade < 18) {
+            menor += 1;
+            frase = "Pessoa cliente menor de idade, catraca liberada!";
+          } else if (idade < 50) {
+            adulta += 1;
+            frase = "Pessoa adulta, catraca liberada!";
+          } else {
+            idoso += 1;
+            frase = "Pessoa adulta maior de 50, catraca liberada!";
+          }
+          System.out.println(frase);
+          break;
+        case 2:
+          int total = menor + adulta + idoso;
+          double menorPorcentagem = (menor * 100.0 / total);
+          double adultoPorcentagem = (adulta * 100.0 / total);
+          double idosoPorcentagem = (idoso * 100.0 / total);
+          System.out.println("----- Quantidade -----");
+          System.out.println("menores: " + menor);
+          System.out.println("adultas: " + adulta);
+          System.out.println("maiores que 50: " + idoso);
 
-        if (idade < 18) {
-          menor += 1;
-          frase = "Pessoa cliente menor de idade, catraca liberada!";
-        } else if (idade <= 50) {
-          adulta += 1;
-          frase = "Pessoa adulta, catraca liberada!";
-        } else {
-          idoso += 1;
-          frase = "Pessoa adulta maior de 50, catraca liberada!";
-        }
-        System.out.println(frase);
+          System.out.println("----- Percentual -----");
+          System.out
+              .println("menores: " + String.format(Locale.US, "%.2f", menorPorcentagem) + "%");
+          System.out
+              .println("adultas: " + String.format(Locale.US, "%.2f", adultoPorcentagem) + "%");
+          System.out.println(
+              "maiores que 50: " + String.format(Locale.US, "%.2f", idosoPorcentagem) + "%");
+          System.out.println("TOTAL: " + total);
+          break;
+        default:
+          System.out.println("Entre com uma opção válida!");
       }
-      int total = menor + adulta + idoso;
-      double menorPorcentagem = (menor * 100 / total);
-      double adultoPorcentagem = (adulta * 100 / total);
-      double idosoPorcentagem = (idoso * 100 / total);
-      if (opcao == 2) {
-
-        System.out.println("----- Quantidade -----");
-        System.out.println("menores: " + menor);
-        System.out.println("adultas: " + adulta);
-        System.out.println("maiores que 50: " + idoso);
-
-        System.out.println("----- Percentual -----");
-        System.out.println("menores: " + menorPorcentagem);
-        System.out.println("adultas: " + adultoPorcentagem);
-        System.out.println("maiores que 50: " + idosoPorcentagem);
-        System.out.println("TOTAL: " + total);
-      }
-
     } while (opcao != 2);
     scanner.close();
   }
